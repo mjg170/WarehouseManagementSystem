e7e31c71bea5c1472035e3e15fc0013cba4c6e87
mjg170 <33008430+mjg170@users.noreply.github.com>
2017-12-01 14:17:27 -0500

    added check in/out stuff and map.  Needs testing

--- Map.java
+++ Map.java
@@ -1,4 +1,19 @@
 
+//package com.javaworld.media.j2d;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.util.List;
+import java.util.ArrayList;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
+
+import javax.imageio.ImageIO;
+
 public class Map {
 	//what floor the map depicts
 	private int floor;
@@ -6,11 +21,67 @@ public class Map {
 	private String name;
 	//scale of map (to determine size)
 	private int scale;
+	
+	BufferedImage img = null;
 
 	//instantiation
 	public Map(String myName, Integer myFloor, Integer myScale) {
 		floor = myFloor;
 		name = myName;
 		scale = myScale;
+	}
+	
+	//changes name of map
+	public void changeName(String newName){
+		name = newName;
+	}
+	
+	//makes new panel for map
+	public void show(String fileName) {
+        JFrame frame = new JFrame();
+        frame.add(new mapPanel(fileName));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+	
+    //actual map panel
+	public class mapPanel extends JPanel{
+		//point where item is
+		private Point point;
+		//background image
+        private BufferedImage image;
+
+        public mapPanel(String fileName) {
+            try {
+                image = ImageIO.read(new File(fileName));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+            //checks if user clicks
+            addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    point = (e.getPoint());
+                    repaint();
+                }                
+            });
+        }
+
+        //draws dot
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g.create();
+            if (image != null) {
+                g2d.drawImage(image, 0, 0, this);
+            }
+            g2d.setColor(Color.RED);
+            
+            g2d.fillOval(point.x - 4, point.y - 4, 8, 8);
+            
+            g2d.dispose();
+        }
 	}
 }
